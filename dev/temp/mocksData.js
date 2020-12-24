84ab1cd997f5c728227cf4b964b10b4455a507ed
ftl222 <7t0oNRgbsYYu9ilsovak>
2020-12-24 19:03:24 +0300

    add build

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,10 @@
+__pages: [{
+                name: 'empty-page',
+                href: 'empty-page.html'
+             },{
+                name: 'index',
+                href: 'index.html'
+             },{
+                name: 'registration-page',
+                href: 'registration-page.html'
+             }]
