181128ee0b88e16fa09394998d5b7762ef5c8e3c
ftl222 <7t0oNRgbsYYu9ilsovak>
2020-12-24 18:55:35 +0300

    fix

--- markup/components/tabs/tabs.js
+++ markup/components/tabs/tabs.js
@@ -1,12 +1,16 @@
 (function () {
     document.querySelectorAll('.tabs__item').forEach(function (el, idx) {
-        el.addEventListener('click', function(e) {
-            document.querySelectorAll('.tabs__item').forEach(function(el) {
+        el.addEventListener('click', function (e) {
+            document.querySelectorAll('.tabs__item').forEach(function (el) {
                 el.classList.remove('is-active')
             });
             e.target.closest('.tabs__item').classList.add('is-active');
 
             const blockWrapper = document.querySelector('.tabs');
+            document.querySelectorAll('.table-wrapper ').forEach(function (el) {
+                el.classList.remove('is-active')
+            })
+            document.querySelectorAll('.table-wrapper ')[idx].classList.add('is-active')
             if (idx === 0) {
                 blockWrapper.classList.remove('tabs--light-blue')
                 blockWrapper.classList.remove('tabs--blue')
