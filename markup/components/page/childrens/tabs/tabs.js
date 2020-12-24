58c28b61b2f02d1c143ba5a51383a1ac58941136
ftl222 <7t0oNRgbsYYu9ilsovak>
2020-12-24 13:23:48 +0300

    add media queries and animations

--- markup/components/page/childrens/tabs/tabs.js
+++ markup/components/page/childrens/tabs/tabs.js
@@ -1,8 +1,22 @@
 (function () {
-    document.querySelectorAll('.tabs__item').forEach(el => {
+    document.querySelectorAll('.tabs__item').forEach((el, idx) => {
         el.addEventListener('click', (e) => {
-            document.querySelectorAll('.tabs__item').forEach(el=> el.classList.remove('is-active'));
-            e.target.closest('.tabs__item').classList.add('is-active')
+            document.querySelectorAll('.tabs__item').forEach(el => el.classList.remove('is-active'));
+            e.target.closest('.tabs__item').classList.add('is-active');
+
+            const blockWrapper = document.querySelector('.tabs');
+            if (idx === 0) {
+                blockWrapper.classList.remove('tabs--light-blue')
+                blockWrapper.classList.remove('tabs--blue')
+            }
+            if (idx === 1) {
+                blockWrapper.classList.remove('tabs--blue')
+                blockWrapper.classList.add('tabs--light-blue')
+            }
+            if (idx === 2) {
+                blockWrapper.classList.add('tabs--blue')
+                blockWrapper.classList.remove('tabs--light-blue')
+            }
         })
     })
 })()
